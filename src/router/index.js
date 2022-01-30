9005df40c5f522d95a2706be0e468b840a1bbcdb
Mel <meljohnpualon@gmail.com>
2022-01-31 02:46:00 +0800

    added not found page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Jobs from '../views/jobs/Jobs.vue'
 import JobDetails from '../views/jobs/JobDetails.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -29,6 +30,12 @@ const routes = [
   {
     path: '/all-jobs',
     redirect: '/jobs'
+  },
+  // Catch All 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
