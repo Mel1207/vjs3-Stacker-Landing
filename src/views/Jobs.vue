c2a094930a799b021564e1645a133a77d69c4606
Mel <meljohnpualon@gmail.com>
2022-01-31 01:45:05 +0800

    added jobs page and data binded components

--- src/views/Jobs.vue
+++ src/views/Jobs.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="jobs">
-    <h1>This is an jobs page</h1>
+    <h1>Job List</h1>
+    <div v-for="job in jobs" :key="job.id">
+      <h2>{{ job.title }}</h2>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [
+        {title: 'Frontend Developer', id: 1, details: 'lorem'},
+        {title: 'Backend Developer', id: 2, details: 'lorem ipsum'},
+        {title: 'UI-UX Designer', id: 3, details: 'lorem dolor'},
+      ]
+    }
+  }
+}
+</script>
