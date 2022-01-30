beddac60738161bd855021955b226b30adce7906
Mel <meljohnpualon@gmail.com>
2022-01-31 01:30:09 +0800

    added footer files and component structure

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <Navigation />
   <router-view/>
+  <Footer />
 </template>
 
 <script>
 import Navigation from './components/Navigation/Navigation.vue'
+import Footer from './components/Footer/Footer.vue'
 
 export default {
-  components: { Navigation }
+  components: { Navigation, Footer }
 }
 </script>
---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer class="footer">
+        <p>Copyright @stacker 2022</p>
+    </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
