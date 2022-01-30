1473b3c42d21a702628607a458504eebee1cbc47
Mel <meljohnpualon@gmail.com>
2022-01-31 02:58:36 +0800

    added router directives and history

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <Navigation />
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go Back</button>
+  <button @click="forward">Go Forward</button>
+
   <router-view/>
   <Footer />
 </template>
@@ -9,6 +14,17 @@ import Navigation from './components/Navigation/Navigation.vue'
 import Footer from './components/Footer/Footer.vue'
 
 export default {
-  components: { Navigation, Footer }
+  components: { Navigation, Footer },
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'Home'})
+    },
+    back() {
+      this.$router.go(-1)
+    },
+    forward() {
+      this.$router.go(1)
+    }
+  }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <h1>This is Home page</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates qui cum voluptatibus excepturi quis ea quasi eius. Temporibus rem aliquam quis laboriosam facilis quasi officiis dicta quae, ipsa consequatur modi tenetur sit mollitia exercitationem adipisci rerum quo! Ipsum praesentium laudantium perspiciatis delectus distinctio, fugit reprehenderit ad repellendus quam tenetur quos!</p><br>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates qui cum voluptatibus excepturi quis ea quasi eius. Temporibus rem aliquam quis laboriosam facilis quasi officiis dicta quae, ipsa consequatur modi tenetur sit mollitia exercitationem adipisci rerum quo! Ipsum praesentium laudantium perspiciatis delectus distinctio, fugit reprehenderit ad repellendus quam tenetur quos!</p>
   </div>
 </template>
 
