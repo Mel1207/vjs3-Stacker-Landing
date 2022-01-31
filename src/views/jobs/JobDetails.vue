8c08121a393758d45fea761a42e8144715bd17cf
Mel <meljohnpualon@gmail.com>
2022-01-31 16:04:54 +0800

    added fetch data from external file

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,16 +1,28 @@
 <template>
     <h1>Job Details Page</h1>
-    <p>The Job ID is {{ id }}</p>
-    <p>Details: {{ details }}</p>
+    <div v-if="job">
+        <h2>{{ job.title }}</h2>
+        <p>The Job ID is {{ id }}</p>
+        <p>Details: {{ job.details }}</p>
+    </div>
+    <div v-else>
+        <p>Loading Job details</p>
+    </div>
 </template>
 
 <script>
 export default {
     props: ['id', 'details'],
-    // data() {
-    //     return {
-    //         id: this.$route.params.id
-    //     }
-    // }
+    data() {
+        return {
+            job: null
+        }
+    },
+    mounted() {
+        fetch('http://localhost:3000/jobs/' + this.id)
+            .then(respose => respose.json())
+            .then(data => this.job = data)
+            .catch(err => console.log(err.message))
+    }
 }
 </script>
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -2,7 +2,7 @@
   <div class="jobs">
     <h1>Job List</h1>
     <div v-for="job in jobs" :key="job.id" class="job">
-      <router-link :to="{ name: 'JobDetails', params: {id: job.id, details: job.details}}">
+      <router-link :to="{ name: 'JobDetails', params: {id: job.id}}">
         <h2>{{ job.title }}</h2>
       </router-link>
     </div>
@@ -13,12 +13,14 @@
 export default {
   data() {
     return {
-      jobs: [
-        {title: 'Frontend Developer', id: 1, details: 'lorem'},
-        {title: 'Backend Developer', id: 2, details: 'lorem ipsum'},
-        {title: 'UI-UX Designer', id: 3, details: 'lorem dolor'},
-      ]
+      jobs: []
     }
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs')
+      .then(respose => respose.json())
+      .then(data => this.jobs = data)
+      .catch(err => console.log(err.message))
   }
 }
 </script>
